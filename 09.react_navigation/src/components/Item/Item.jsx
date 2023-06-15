a30e24c404b576b6112f91b25380fe350bd095be
Tathiane Noriko <tathianenoriko@gmail.com>
2023-06-15 14:11:54 -0300

    add pratica 09

---
+++ 09.react_navigation/src/components/Item/Item.jsx
@@ -0,0 +1,16 @@
+function Item({ item, onItemClick }) {
+  const handleClick = () => {
+    onItemClick && onItemClick(item);
+  };
+
+  return (
+    <li>
+      <p>{item.name}</p>
+      <button type="button" onClick={handleClick}>
+        Ver detalhes
+      </button>
+    </li>
+  );
+}
+
+export default Item;
---
+++ 09.react_navigation/src/components/List/List.jsx
@@ -0,0 +1,16 @@
+import Item from "../Item";
+
+function List({ items = [] }) {
+  const map = items.map((item) => {
+    return <Item key={item.id} item={item} />;
+  });
+
+  return (
+    <div className="title">
+      <h2>Lista de Usuários</h2>
+      <ul>{map}</ul>
+    </div>
+  );
+}
+
+export default List;
