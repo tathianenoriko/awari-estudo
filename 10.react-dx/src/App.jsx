a5dfe7081f3a5601a16d701a28113c5094bff557
Tathiane Noriko <tathianenoriko@gmail.com>
2023-06-16 18:50:23 -0300

    add pratica 10

---
+++ 10.react-dx/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import { Router } from "./components";
+
+const App = () => {
+  return <Router />;
+};
+
+export default App;
---
+++ 10.react-dx/src/components/List/List.jsx
@@ -0,0 +1,20 @@
+import Item from "../Item";
+
+function List({ items = [], onItemClick }) {
+  const handleItemClick = (item) => {
+    onItemClick && onItemClick(item);
+  };
+
+  const map = items.map((item) => {
+    return <Item key={item.id} item={item} onItemClick={handleItemClick} />;
+  });
+
+  return (
+    <div className="title">
+      <h2>Lista de Usuários</h2>
+      <ul>{map}</ul>
+    </div>
+  );
+}
+
+export default List;
