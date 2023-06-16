0e0fa6836781c5e8aff004f484d9b5ae122ebab9
Tathiane Noriko <tathianenoriko@gmail.com>
2023-06-16 17:42:57 -0300

    update

--- 09.react_navigation/src/components/List/List.jsx
+++ 09.react_navigation/src/components/List/List.jsx
@@ -1,8 +1,12 @@
 import Item from "../Item";
 
-function List({ items = [] }) {
+function List({ items = [], onItemClick }) {
+  const handleItemClick = (item) => {
+    onItemClick && onItemClick(item);
+  };
+
   const map = items.map((item) => {
-    return <Item key={item.id} item={item} />;
+    return <Item key={item.id} item={item} onItemClick={handleItemClick} />;
   });
 
   return (
